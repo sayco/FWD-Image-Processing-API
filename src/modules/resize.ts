fc75261796fa6978059fd2638e09024b4d2063db
sayco <sayco.only@gmail.com>
2022-11-14 23:48:01 +0200

    ADD request queries

--- src/modules/resize.ts
+++ src/modules/resize.ts
@@ -1,22 +1,23 @@
-//import sharp from "sharp";
+import sharp from "sharp";
 import { promises as fs } from "fs";
 
+let input = fs.open("../imgs/input/*.jpg");
 
 
 const resize = async () => {
-  sharp(input)
-  .resize(200, 300, {
-    kernel: sharp.kernel.nearest,
-    fit: 'contain',
-    position: 'right top',
-    background: { r: 255, g: 255, b: 255, alpha: 0.5 }
-  })
-  .toFile('output.png')
-  .then(() => {
-    // output.png is a 200 pixels wide and 300 pixels high image
-    // containing a nearest-neighbour scaled version
-    // contained within the north-east corner of a semi-transparent white canvas
-  });
+  // sharp(input)
+  // .resize(200, 300, {
+  //   kernel: sharp.kernel.nearest,
+  //   fit: 'contain',
+  //   position: 'right top',
+  //   background: { r: 255, g: 255, b: 255, alpha: 0.5 }
+  // })
+  // .toFile('output.png')
+  // .then(() => {
+  //   // output.png is a 200 pixels wide and 300 pixels high image
+  //   // containing a nearest-neighbour scaled version
+  //   // contained within the north-east corner of a semi-transparent white canvas
+  // });
 }
 
 
--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -5,6 +5,9 @@ const routes = express.Router();
 
 routes.get("/", (req, res) => {
   res.send("Welcome to Resize API.");
+  console.log(req.query.fullname);
+  console.log(req.query.hight);
+  console.log(req.query.width);
 })
 
 
