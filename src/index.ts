e544f3bf55cd2951803a4be782c53acaf48edab9
sayco <sayco.only@gmail.com>
2022-11-20 00:09:11 +0200

    Logger Added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import routes from "./routes/index";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
 
 const port = 3000;
 const app = express();
+let logStream = fs.createWriteStream(path.join("./", "api-logger.log"), {
+  flags: 'a'
+})
 
+app.use(morgan(':method :url :status - :response-time ms', {
+  stream: logStream
+}));
 app.use("/api", routes);
 
 app.listen(port, () => {
