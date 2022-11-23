5f7decfc33f8c82f294038a59fe8282b1199ed13
sayco <sayco.only@gmail.com>
2022-11-23 23:00:00 +0200

    ADD chaching

--- src/modules/resizeImage.ts
+++ src/modules/resizeImage.ts
@@ -15,34 +15,21 @@ import sharp from 'sharp';
    * if not exist will resize then send it
    */
 async function resizeImage(res:express.Response, inputFile: string, outputFile: string, resizeOption:resizeOptionObj):Promise<void>{
-    try {
-      // check if file exist
-      const imageData = await fs.access(outputFile);
-      
-      // file exist
-      // send the file only
-      // console.log("Resized Image file is Already Exist.");
-      sharp(outputFile)
-        .toBuffer()
-        .then((data) => {
-          res.type("image/jpeg");
-          res.send(data);
-        });
-    } catch (error) {
-      // file dose not exist
-      // do the resize process
-      //console.log(error);
-      // console.log("Resized Image File dose not exist, will create one.");
+    
+  try {
       sharp(inputFile)
-        .resize(resizeOption)
-        .jpeg()
-        .toBuffer()
-        .then((data) => {
-          res.type("image/jpeg");
-          res.send(data);
-          fs.writeFile(outputFile, data);
-        });
-    }
+      .resize(resizeOption)
+      .jpeg()
+      .toBuffer()
+      .then((data) => {
+        res.type("image/jpeg");
+        res.status(200).send(data);
+        fs.writeFile(outputFile, data);
+      });
+    } catch (error) {
+      console.log(error);
+  }
+  
    }
   
 export default resizeImage;
--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -26,9 +26,6 @@ routes.get("/", (req, res) => {
     height?: number;
  }
 
-  console.log(height);
-  console.log(heightQuery);
-  console.log(Number.isInteger(height));
   // make sure at least one of resize dimensions are exist
   // check if no queries provided by the user
   if (
@@ -36,50 +33,46 @@ routes.get("/", (req, res) => {
     heightQuery === undefined &&
     widthQuery === undefined
   ) {
-    res.send(
+    res.status(404).send(
       "Welcome to Resize API, kindly Add image file name and the required dimensions."
     );
     return;
   }
   // check if file name is missing
   else if (filename === undefined) {
-    //console.log("File Name is missing.");
-    res.send("Image file name is missing.");
+    res.status(404).send("Image file name is missing.");
     return;
   }
   // check if both height and width are missing
   else if (heightQuery === undefined && widthQuery === undefined) {
-    // console.log("Both Width and Height are missing.");
-    res.send("Image Hight / Width are missing, kindly send at least one.");
+    res.status(404).send("Image Hight / Width are missing, kindly send at least one.");
     return;
   }
   else if (!(Number.isInteger(width) && Number.isInteger(height))) {
-    res.send("Image Hight / Width must be numbers, kindly send at least one.");
+    res.status(404).send("Image Hight / Width must be numbers, kindly send at least one.");
     return;
   }
   else if (!(Number.isInteger(width))) {
-    res.send("Image Width must be a numbers, kindly send at least one.");
+    res.status(404).send("Image Width must be a numbers, kindly send at least one.");
     return;
   }
   else if (!(Number.isInteger(height))) {
-    res.send("Image Height must be a numbers, kindly send at least one.");
+    res.status(404).send("Image Height must be a numbers, kindly send at least one.");
     return;
   }
   else if (!(width > 0 && height > 0)) {
-    res.send("Image Hight / Width must greater than zero, kindly send at least one.");
+    res.status(404).send("Image Hight / Width must greater than zero, kindly send at least one.");
     return;
   }
   // check if height is missing
   else if (heightQuery === undefined) {
     checkResizedImageExistence();
-    // console.log("Height is missing");
     resizeOption = { width: width };
     return;
   }
   // check if width is missing
   else if (widthQuery === undefined) {
     checkResizedImageExistence();
-    // console.log("Width is missing");
     resizeOption = { height: height };
     return;
   }
@@ -96,7 +89,7 @@ routes.get("/", (req, res) => {
       // if resized image exist will send cached image
       // if not found will resize the image then send it
       if (resizedImageAvaliable) {
-        res.sendFile(path.join(__dirname, `../../../images/thumbnail/${filename}_[${width}x${height}]_thumb.jpg`));
+        res.status(304).sendFile(path.join(__dirname, `../../../images/thumbnail/${filename}_[${width}x${height}]_thumb.jpg`));
       } else {
         checkInputImageExistence();
       }
@@ -115,7 +108,7 @@ routes.get("/", (req, res) => {
       if (inputImageAvaliable) {
         resizeImage(res, inputFile, outputFile, resizeOption);
       } else {
-        res.send("Image File name is not found, kindly check file name.");
+        res.status(404).send("Image File name is not found, kindly check file name.");
       }
     } catch (error) {
       console.log(error);
